bc6da5622e42765ae999953b4c539ba62fbb80ce
Abdulsalam Abdulsalam <0xdudul@gmail.com>
2024-08-18 14:45:17 +0000

    feat: PERC20 contract implemented

---
+++ auto.bash
@@ -0,0 +1,28 @@
+npm i
+
+npm run compile
+
+npm run deploy
+npm run deploy
+npm run deploy
+npm run deploy
+npm run deploy
+
+npm run mint
+npm run mint
+npm run mint
+npm run mint
+npm run mint
+
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+
+rm -rf .git
+git init 
+git add .
+git commit -m "feat: PERC20 contract implemented"
+
---
+++ utils/deployed-address.ts
@@ -0,0 +1,3 @@
+const deployedAddress = '0x61b1D8B4f1Df28D9F82D74a4776555D0Ac69CCea'
+
+export default deployedAddress
